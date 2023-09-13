d1be84e1bf0f3356cd95d8ddfcb708712a0fe768
KyrylIeremieiev <34677@ma-web.nl>
2023-09-13 23:29:16 +0200

    add fetch to js, add oop

--- web/app.js
+++ web/app.js
@@ -1,15 +1,38 @@
 
-const chart = document.getElementById('myChart');
-let lineChart = new Chart(chart, {
-        type:'line',
-        data: {
-            labels:["this", "is", "a", "test"],
-            datasets: [
-                {
-                    label:"My first dataset",
-                    data:[1, 2, 3, 4]
-                }
-            ]
-        }
+class App{
+    constructor(){
+        this.getData().then(()=>{
+            console.log(this.data);
+            this.drawChart()
+        })
+        
     }
-    );
+    
+    async getData(){
+        await fetch("http://localhost:3030/fetch").then(response =>{
+            return response.json();
+            }).then(data =>{
+                this.data = data;
+            });
+
+    }
+
+    drawChart(){
+        const chart = document.getElementById('myChart');
+        this.lineChart = new Chart(chart, {
+                type:'line',
+                data: {
+                    labels:["this", "is", "a", "test"],
+                    datasets: [
+                        {
+                            label:"Moisture Level (%)",
+                            data:[this.data[0][1], this.data[1][1], this.data[2][1], this.data[3][1]]
+                        }
+                    ]
+                }
+            }
+            );
+    }
+}
+
+let app = new App();
