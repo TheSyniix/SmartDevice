d668461c6c4bf2ffc081be47994693d4bab258db
TheSyniix <brandonreckerts@hotmail.com>
2023-09-14 21:07:36 +0200

    added corrected version of project script

--- rev6/Vochtsensor.py
+++ rev6/Vochtsensor.py
@@ -15,22 +15,27 @@ url = "http://localhost:3030/insert"
 
 # GPIO pin servo motor
 servo_pin = 22
+LED1 = 18
+LED2 = 12
+LED3 = 17
+LED4 = 16
 
 #LED stup
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED1,GPIO.OUT)
+GPIO.setup(LED2,GPIO.OUT)
+GPIO.setup(LED3,GPIO.OUT)
+GPIO.setup(LED4,GPIO.OUT)
 GPIO.setwarnings(False)
-GPIO.setup(12,GPIO.OUT)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
 GPIO.setup(servo_pin, GPIO.OUT)
 
 # Connect Moisture Sensor to MCP3008 kanaal0
 moisture_sensor = MCP3008(channel=0)
 
 # Define function convert ADC value naar percentage
-def convert_to_percentage(value, max_value= 0.6):
+def convert_to_percentage(value, max_value= 0.7):
     return (value / max_value) * 100
+
 # PWM servo
 servo_pwm = GPIO.PWM(servo_pin, 50)  # 50 Hz PWM frequency
 
@@ -82,23 +87,27 @@ ax.set_xlim(0, len(value_list))
 ax.set_ylim(0, 1.0)  
 
 try:
+    start_tijd = time.time()
     while True:
+        verlopen_tijd = time.time() - start_tijd
+               
+        
         # Input moisture value en omzetten in percentage
         moisture_value = moisture_sensor.value
         moisture_percentage = convert_to_percentage(moisture_value)
         
         #opslaan values and timestamps
         value_list.append(moisture_percentage)
-        timestamps.append(time.time())
+        timestamps.append(verlopen_tijd)
         
         # Print de moisture percentage in console
-        #print(f"Moisture Level: {moisture_percentage:.2f}%")
+        print(f"Moisture Level: {moisture_percentage:.2f}%")
         
         # lijst limiet
-        max_points = 100
+        max_points = 250
         if len(value_list) > max_points:
             value_list.pop(0)
-            timestamps.pop(0)
+            #timestamps.pop(0)
 
         # Update the live plot
         line.set_data(range(len(value_list)), value_list)
@@ -109,10 +118,10 @@ try:
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
         # Check if the moisture value is below the threshold        
-        draw.text((x, top + 0), f"Moisture Level: {moisture_percentage:.2f}%", font=font, fill=255)
+        draw.text((x, top + 0), f"Vochtigheid: {moisture_percentage:.2f}%", font=font, fill=255)
         
         if moisture_percentage < threshold:
-            print("Water geven")
+            print("Geef me water")
             draw.text((x, top + 12), f"geef me water", font=font, fill=255)
             GPIO.output(18, True)
             GPIO.output(17, False)
@@ -122,7 +131,7 @@ try:
             time.sleep(0.5)  # Wait for the servo to move
                         
         elif 21 <= moisture_percentage <= 40: 
-            print("Aan het drinken")
+            print("beetje dorstig")
             draw.text((x, top + 12), f"Geef me meer", font=font, fill=255)
             GPIO.output(12, True)
             GPIO.output(17, False)
@@ -130,7 +139,7 @@ try:
             GPIO.output(16, False)
             
         elif 41 <= moisture_percentage <= 60: 
-            print("Aan het drinken")
+            print("Voel me goed")
             draw.text((x, top + 12), f"goed", font=font, fill=255)
             GPIO.output(12, False)
             GPIO.output(17, True)
@@ -138,7 +147,7 @@ try:
             GPIO.output(16, False)
                         
         else:   
-            print("hydrated")
+            print("Ik verdrink!!")
             draw.text((x, top + 12), f"blessed", font=font, fill=255)
             GPIO.output(17, False)
             GPIO.output(18, False)
@@ -165,17 +174,17 @@ except KeyboardInterrupt:
     # Exit the program when Ctrl+C is pressed
     pass
 
-finally:
+#finally:
     #cleanen van gpio poorten
-    servo_pwm.stop()
-    GPIO.cleanup()
+servo_pwm.stop()
+GPIO.cleanup()
  
 #plotten van grafiek
 plt.figure()
 plt.plot(timestamps, value_list)
 plt.xlabel("Tijd in sec")
-plt.ylabel("Moisture Level (%)")
-plt.title("Moisture Level Over Time")
+plt.ylabel("Vochtigheidsniveau (%)")
+plt.title("Vochtigheid over tijd")
 plt.grid(True)
 
 #send request to api
@@ -185,7 +194,9 @@ for x in value_list:
     x = requests.post(url, json = myobj)
     current_timestamp+=1
 
-
 # Save the plot as a PDF file
 plt.savefig("moisture_plot.pdf")
 plt.show()
+
+#print(timestamps)
+print(verlopen_tijd)
